57f540b29c16726148640972bcff6bc8b42c4c43
JoaoVictorQuadrosLopes <jvqlopes@minha.fag.edu.br>
2026-05-13 17:09:47 -0300

    att

---
+++ backend/src/db/connection.js
@@ -0,0 +1,9 @@
+const {pool} = require('./pool');
+
+const pool = new Pool ({
+    connectionString: process.env.DATABASE_URL,
+});
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+};
