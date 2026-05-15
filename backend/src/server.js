22fa17dc55c51b2836a8689cc780e389fba18132
JoaoVictorQuadrosLopes <jvqlopes@minha.fag.edu.br>
2026-05-15 12:02:01 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,6 +11,13 @@ const app = Fastify({
   logger: true
 });
 
+// são rotas, muitas rotas puxando da api, 
+// e cada rota tem um prefixo, tipo:
+// /api/auth, /api/calculations
+// E cada rota tem um arquivo separado, tipo authRoutes.js, calculationRoutes.js
+// Arquivos de rota tem as rotas relacionadas a ele, tipo authRoutes.js tem as rotas de login e cadastro, calculationRoutes.js tem as rotas de cálculo e histórico
+// Cada rota tem um handler que é a função que vai ser executada quando a rota for chamada. 
+// O handler tem acesso ao request e reply do Fastify, e pode usar o db para acessar o banco de dados, e pode usar o app.jwt para gerar ou verificar tokens JWT
 app.register(cors, {
   origin: true
 });
